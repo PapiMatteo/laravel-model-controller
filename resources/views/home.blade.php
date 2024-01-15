feff2430b63c3e8dd7c53524f62eec8ce300a356
PapiMatteo <matteopapi.work@gmail.com>
2024-01-15 13:45:14 +0100

    add: data in home and style implemented

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+    <div class="container">
+        <div class="row row-cols-2 gy-3">
+            @foreach ($movies as $movie)
+                 <div class="col">
+                    <div class="card">
+                        <img src="{{ $movie->image }}" alt="">
+                        <h3 class="text-center">{{ $movie->title }}</h3>
+                        <p class="text-center">{{ $movie->original_title }}</p>
+                        <p class="danger text-center">{{ $movie->vote }}</p>
+                    </div>
+                </div>
+            @endforeach
+           
+        </div>
+    </div>
 @endsection
